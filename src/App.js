3c8991e031cd5b49abccaa05b8d54f74107539fa
juliahindle <julia.hindle@gmail.com>
2022-09-28 00:32:53 -0500

    Google form API integration

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Home from 'components/pages/Home'
 import About from 'components/pages/About'
 import Resources from 'components/pages/Resources'
 import Glazes from 'components/pages/Glazes'
+import auth2 from 'data/ceramics-363805-f0fc817c49ec.json'
 // Constants
 import { BLANK_GLAZE, GlazesContext } from 'Constants'
 
@@ -21,21 +22,17 @@ function App() {
 
     // User tracking
     const getData = async () => {
-        const res = await axios.get('https://geolocation-db.com/json/')
-        var resData = res.data
+        const geoResponse = await axios.get('https://geolocation-db.com/json/')
+        var geoData = geoResponse.data
 
-        var auth = "ya29.a0Aa4xrXO_thddrv18_oCXBEkvXGzMmiSS67_XwW6ftSwOcizA4q1Zl31Za0Ch6aLYpYbQMzo3GdjtyRFIowXkRyBReijKhKmFXIomM8zGU9HzzHLpzo2ZcPcWgsDyq6jSyR8KHNEe3_M6I8mm_NkHgVOnN5i3aCgYKATASARISFQEjDvL9xsS24VzrWQyZy63T0LTetA0163"
-        
-        var test = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/1JdWa8Ug7KOUkhVlFoI6IoSWPGz3Sk1htqCqA69BTYAo/values/Sheet1!A%3AJ:append?insertDataOption=INSERT_ROWS&valueInputOption=RAW`,{
-            method: 'POST',
-            headers:{
-                "Authorization": `Bearer ${auth}`,
-                'content-type':'application/json'
-            },
-            body: JSON.stringify({"range": "Sheet1!A:J","values": [[window.location.host, resData.IPv4, resData.city,resData.country_code,resData.country_name,resData.state,browserName,browserVersion,window.innerHeight,window.innerWidth]]})
-        })
-        var response = await test.json()
-        console.log(response)
+        let url = `https://docs.google.com/forms/u/0/d/e/1FAIpQLSfZ07yxNyO6q-KGF2Np2JxZwHfSpP_GLq3VhBKA8rtV99YBTA/formResponse?entry.1316452331=${window.location.host}&entry.963390676=${geoData.IPv4}&entry.852573067=${geoData.city}&entry.1495514100=${geoData.country_code}&entry.1375356915=${geoData.country_name}&entry.1116749267=${geoData.state}&entry.1578103802=${browserName}&entry.1601834397=${browserVersion}&entry.110367476=${window.innerHeight}&entry.1601881770=${window.innerWidth}`;
+
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        });
     }
 
     useEffect(() => { getData() }, [])
