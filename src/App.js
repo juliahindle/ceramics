4c3e41690f05b07c15cb284241cfd502d4f30776
juliahindle <julia.hindle@gmail.com>
2022-10-23 03:52:54 -0500

    Create in progress page and fix null track data

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Home from 'components/pages/Home'
 import About from 'components/pages/About'
 import Resources from 'components/pages/Resources'
 import Glazes from 'components/pages/Glazes'
+import InProgress from 'components/pages/InProgress'
+
 // Constants
 import { BLANK_GLAZE, GlazesContext } from 'Constants'
 
@@ -24,7 +26,9 @@ function App() {
         const geoResponse = await axios.get('https://geolocation-db.com/json/')
         var geoData = geoResponse.data
 
-        let url = `https://docs.google.com/forms/u/0/d/e/1FAIpQLSfZ07yxNyO6q-KGF2Np2JxZwHfSpP_GLq3VhBKA8rtV99YBTA/formResponse?entry.1316452331=${window.location.host}&entry.963390676=${geoData.IPv4}&entry.852573067=${geoData.city}&entry.1495514100=${geoData.country_code}&entry.1375356915=${geoData.country_name}&entry.1116749267=${geoData.state}&entry.1578103802=${browserName}&entry.1601834397=${browserVersion}&entry.110367476=${window.innerHeight}&entry.1601881770=${window.innerWidth}`
+        let city = geoData.city || ""
+        let state = geoData.state || ""
+        let url = `https://docs.google.com/forms/u/0/d/e/1FAIpQLSfZ07yxNyO6q-KGF2Np2JxZwHfSpP_GLq3VhBKA8rtV99YBTA/formResponse?entry.1316452331=${window.location.host}&entry.963390676=${geoData.IPv4}&entry.852573067=${city}&entry.1495514100=${geoData.country_code}&entry.1375356915=${geoData.country_name}&entry.1116749267=${state}&entry.1578103802=${browserName}&entry.1601834397=${browserVersion}&entry.110367476=${window.innerHeight}&entry.1601881770=${window.innerWidth}`
 
         fetch(url, {
             method: "POST",
@@ -53,7 +57,7 @@ function App() {
                             </GlazesContext.Provider>
                         } 
                     />
-                    <Route path="/about" element={<About />} />
+                    <Route path="/about" element={<InProgress />} />
                     <Route path="/resources" element={<Resources />} />
                 </Route>
             </Routes>
---
+++ src/components/pages/InProgress.js
@@ -0,0 +1,9 @@
+function InProgress() {
+    return (
+        <div className='in-progress'>
+            Page not made yet! Currently working on it.
+        </div>
+    )
+}
+
+export default InProgress
