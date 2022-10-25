2690530ed41362460034838eba89d8f9890817a1
juliahindle <julia.hindle@gmail.com>
2022-10-24 21:37:54 -0500

    Responsive design for home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,38 +6,44 @@ function Home() {
 
     return (<>
         <section className="home">
-            <img 
-                className="icon filled teapot"
-                src={"images/home/teapot-filled.svg"}
-                alt={"teapot icon with color"}
-            />
-            <img 
-                className="icon outline teapot"
-                src={"images/home/teapot-outline.svg"}
-                alt={"teapot icon outline"}
-            />
-            <img 
-                className="icon filled vase"
-                src={"images/home/vase-filled.svg"}
-                alt={"vase icon with color"}
-            />
-            <img 
-                className="icon outline vase"
-                src={"images/home/vase-outline.svg"}
-                alt={"vase icon outline"}
-            />
-            <h1>Julia Hindle Ceramics</h1>
-            <ul>
-                <li>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/resources">Resources</NavLink>
-                </li>
-                <li>
-                    <NavLink to={{pathname: "/glazes"}}>Glazes</NavLink>
-                </li>
-            </ul>
+            <div className="image teapot">
+                <img 
+                    className="icon filled teapot"
+                    src={"images/home/teapot-filled.svg"}
+                    alt={"teapot icon with color"}
+                />
+                <img 
+                    className="icon outline teapot"
+                    src={"images/home/teapot-outline.svg"}
+                    alt={"teapot icon outline"}
+                />
+            </div>
+            <div className="content">
+                <h1>Julia Hindle Ceramics</h1>
+                <ul>
+                    <li>
+                        <NavLink to="/about">About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/resources">Resources</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={{pathname: "/glazes"}}>Glazes</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className="image vase">
+                <img 
+                    className="icon filled vase"
+                    src={"images/home/vase-filled.svg"}
+                    alt={"vase icon with color"}
+                />
+                <img 
+                    className="icon outline vase"
+                    src={"images/home/vase-outline.svg"}
+                    alt={"vase icon outline"}
+                />
+            </div>
         </section>
     </>)
 }
