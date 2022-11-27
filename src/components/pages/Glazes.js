cc699c1f6b6f8e7303e9061213a103f0a9bafd0c
juliahindle <julia.hindle@gmail.com>
2022-11-27 01:57:02 -0800

    Base filtering functionality + applied filters UI

--- src/components/pages/Glazes.js
+++ src/components/pages/Glazes.js
@@ -1,15 +1,31 @@
 import 'styles/glazes.scss'
-import React, { useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import glazes from 'data/glazes.json'
 import bases from 'data/bases.json'
 import { BLANK_GLAZE, GlazesContext, updatePageTitle, setScroll, resetScroll } from 'Constants'
 
 function Glazes() {
     const {showSidebar, setShowSidebar, selectedGlaze, setSelectedGlaze} = useContext(GlazesContext)
+    const [searchParams] = useSearchParams()
+    const [filters, setFilters] = useState({colors: "all", bases: "all"})
+    const [filteredGlazes, setFilteredGlazes] = useState(glazes)
 
     useEffect(() => {
         updatePageTitle("Glazes - Julia Hindle Ceramics")
         resetScroll()
+        
+        let queryColors = searchParams.getAll("color")
+        let queryBases = searchParams.getAll("base")
+        setFilters({
+            colors: queryColors ? queryColors : "all",
+            bases: queryBases ? queryBases : "all"
+        })
+        setFilteredGlazes(glazes.filter(glaze => 
+            (queryBases.length == 0 || queryBases.includes(glaze.base))))
+        
+        console.log(queryColors)
+        console.log(queryColors)
     }, [])
 
     useEffect(() => {
@@ -29,18 +45,37 @@ function Glazes() {
         }
     }
 
+    const getCurrentFilters = () => {
+        let filterMarkup = []
+        let colors = filters.colors
+        let bases = filters.bases
+        
+        colors !== "all" && colors.map((filter) => {
+            filterMarkup.push(<div className={filter}><span>x</span> {filter}</div>)
+        })
+
+        bases !== "all" && bases.map((filter) => {
+            filterMarkup.push(<div className={filter}><span>x</span> {filter}</div>)
+        })
+
+        return filterMarkup
+    }
+
     return (<>
         {/* Glazes */}
         <section className="glazes" onClick={handleGlazeContainerClick}>
             <div className="text-container">
-                <button className="filters"><span>+</span> Add filter</button>
+                <div className="filters">
+                    <button className="filters"><span>+</span> Add filter</button>
+                    {getCurrentFilters()}
+                </div>
 
-                {glazes.length == 1 ? 
-                 <p>{glazes.length} glaze</p> :
-                 <p>{glazes.length} glazes</p>}
+                {filteredGlazes.length == 1 ? 
+                 <p>{filteredGlazes.length} glaze</p> :
+                 <p>{filteredGlazes.length} glazes</p>}
             </div>
             <div className="glazes-container">
-                {glazes.map(glaze => 
+                {filteredGlazes.map(glaze => 
                     glaze.status !== 'inactive' && 
                     <button>
                         <img
@@ -72,15 +107,6 @@ function Glazes() {
                 </button>
                 <img src={`images/glazes/2x/${selectedGlaze.glaze.id}.png`} alt={`glaze with id: ${selectedGlaze.glaze.id}`}/> 
 
-                <h2>Cone</h2>
-                <p>Cone 6</p>
-
-                <h2>Base Glaze</h2>
-                <p>{selectedGlaze.glaze.base}</p>
-
-                <h2>Clay</h2>
-                <p>{selectedGlaze.glaze.clay} Stoneware</p>
-
                 <h2>Recipe</h2>
                 <table className="recipe">
                     <tbody>
@@ -103,6 +129,15 @@ function Glazes() {
                         )}
                     </tbody>
                 </table>
+
+                <h2>Cone</h2>
+                <p>Cone 6</p>
+
+                <h2>Base Glaze</h2>
+                <p>{selectedGlaze.glaze.base}</p>
+
+                <h2>Clay</h2>
+                <p>{selectedGlaze.glaze.clay} Stoneware</p>
             </div>
         </aside>
     </>)
