13f47493f8466fc7990d1973ac69e2e0b859fc4e
juliahindle <julia.hindle@gmail.com>
2022-11-30 18:07:13 -0600

    Add Filter populating

--- src/components/pages/glazes/AddFilter.js
+++ src/components/pages/glazes/AddFilter.js
@@ -2,6 +2,8 @@ import 'styles/glazes.scss'
 import React, { useState, useContext, useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import bases from 'data/bases.json'
+import ingredients from 'data/ingredients.json'
+import colors from 'data/colors.json'
 
 function AddFilter({searchParams, setSearchParams, showAddFilter, setShowAddFilter}) {
 
@@ -58,9 +60,29 @@ function AddFilter({searchParams, setSearchParams, showAddFilter, setShowAddFilt
             <span className="title">Add filter</span>
 
             <form className="filter-form">
-                <input className="filter-form" type="checkbox" id="red" name="red" value="Red"/>
-                <label className="filter-form" for="red">Red</label>
+                <h3>Color</h3>
+                {colors.map((color) =>
+                    <label className="filter-form">
+                        <input className="filter-form" type="checkbox" value={color}/>
+                        <span>{color}</span>
+                    </label>
+                )}
+                <h3>Ingredient</h3>
+                {ingredients.map((ingredient) =>
+                    <label className="filter-form">
+                        <input className="filter-form" type="checkbox" value={ingredient}/>
+                        <span>{ingredient}</span>
+                    </label>
+                )}
+                <h3>Base Glaze</h3>
+                {bases.sort((a, b) => a.name.localeCompare(b.name)).map((base) =>
+                    <label className="filter-form">
+                        <input className="filter-form" type="checkbox" value={base.name}/>
+                        <span>{base.name}</span>
+                    </label>
+                )}
             </form>
+            <button className="submit">Apply Changes</button>
         </button>
         {/* Applied filters */}
         <div className="filters">{getCurrentFilters()}</div>
