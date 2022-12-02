2661741e7bb5f3de15064372d3ffff9eb9f34b7b
juliahindle <julia.hindle@gmail.com>
2022-12-01 21:22:18 -0600

    Filters cleanup

--- src/components/pages/glazes/FilterCategory.js
+++ src/components/pages/glazes/FilterCategory.js
@@ -12,29 +12,34 @@ function FilterCategory({
     const ref = useRef(null)
     const {toggleCategory, addFilter, removeFilter, tabbable} = useContext(FilterCategoriesContext)
 
-    const getFilterOption = (value, categoryName) => {
-        return (
-            <label className="filter-form">
-                <input 
-                    className="filter-form" 
-                    type="checkbox" 
-                    checked={checkedOptions.find(optionName => optionName === value)} 
-                    value={value}
-                    tabIndex={tabbable ? undefined : -1}
-                    onClick={(e) => {
-                        e.currentTarget.checked ? addFilter(value, categoryName) : removeFilter(value, categoryName)
-                    }}    
-                />
-                <span>{value}</span>
-            </label>
-        )
-    }
-
     return (<>
-        <h3 className="filter-form" onClick={() => toggleCategory(id)}>{(enabled ? "\u25BE " : "\u25B8 ") + name}</h3>
-        <div ref={ref} className={"category  " + (enabled ? "enabled" : "disabled")} style={{maxHeight: enabled ? ref.current.scrollHeight : 0}}>
-            {options.map(option =>
-                getFilterOption(option, id)
+        {/* Title */}
+        <h3 
+            className="filter-form" 
+            onClick={() => {toggleCategory(id)}}
+        >
+            {(enabled ? "\u25BE " : "\u25B8 ") + name}
+        </h3>
+        
+        <div
+            ref={ref} 
+            className={"category  " + (enabled ? "enabled" : "disabled")} 
+            style={{maxHeight: enabled ? ref.current.scrollHeight : 0}}
+        >
+            {options.map(option => 
+                <label className="filter-form">
+                    <input 
+                        className="filter-form" 
+                        type="checkbox" 
+                        checked={checkedOptions.find(checkedOption => checkedOption === option)} 
+                        value={option}
+                        tabIndex={tabbable ? undefined : -1}
+                        onClick={(e) => {
+                            e.currentTarget.checked ? addFilter(option, name) : removeFilter(option, name)
+                        }}    
+                    />
+                    <span>{option}</span>
+                </label>
             )}
         </div>
     </>)
