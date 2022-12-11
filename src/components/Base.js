0767de8681d00bcaea04f934aca9ce0d1f52593f
juliahindle <julia.hindle@gmail.com>
2022-12-10 23:07:26 -0600

    Fix mobile header

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,29 +1,12 @@
 import 'styles/base.scss'
-import React, { useEffect, useState } from 'react'
 import { Outlet, Link, NavLink } from 'react-router-dom'
 
 function Base({showSidebar}) {
-    const [scale, setScale] = useState(1);
-
-    useEffect(() => {
-        const handleResize = () => {
-            let newScale
-
-            if (window.innerWidth <= 500) newScale = (window.innerWidth + 40)/660
-            else if (window.innerWidth <= 660) newScale = window.innerWidth/660
-            else newScale = 1
-
-            setScale(newScale)
-        }
-        handleResize()
-        window.addEventListener("resize", handleResize)
-        return () => window.removeEventListener("resize", handleResize)
-    }, []);
 
     return (<>
             <header>
                 <nav>
-                    <ul style={{scale: scale.toString()}}>
+                    <ul>
                         <li className="page">
                             <NavLink to="/about">About</NavLink>
                         </li>
