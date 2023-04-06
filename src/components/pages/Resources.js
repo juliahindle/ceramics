452b5a24e527dc5519806285b9097ef4864d2e13
juliahindle <julia.hindle@gmail.com>
2023-04-06 18:39:07 -0500

    Resource redesign that works on chrome
    Doesn't work on safari, still need to debug
    
    Jk I pushed the wrong version of Resources.js

--- src/components/pages/Resources.js
+++ src/components/pages/Resources.js
@@ -16,18 +16,20 @@ function Resources() {
             <div className="container">
                 {resources.map((resource, index) => 
                     resource.status !== "inactive" &&
-                    <div className={"parent index-" + index%5}>
-                        <Link to={"/" + resource.url}>
-                            <div className="text">
-                                <h2>{resource.title}</h2>
-                                <hr></hr>
-                                <p className="description">{resource.description}</p>
-                                <p className="date">{resource.dateAdded}</p>
-                            </div>
-                            <div className="image">
-                                <img src={`images/resources/previews/${resource.url}.png`} alt="resource"/>
-                            </div>
-                        </Link>
+                    <div className='parent'>
+                        <div className={"content index-" + index%6}>
+                            <Link to={"/" + resource.url}>
+                                <div className="text">
+                                    <h2>{resource.title}</h2>
+                                    <hr></hr>
+                                    <p className="description">{resource.description}</p>
+                                    <p className="date">{resource.dateAdded}</p>
+                                </div>
+                                <div className="image">
+                                    <img src={`images/resources/previews/${resource.url}.png`} alt="resource"/>
+                                </div>
+                            </Link>
+                        </div>
                     </div>
                 )}
             </div>
